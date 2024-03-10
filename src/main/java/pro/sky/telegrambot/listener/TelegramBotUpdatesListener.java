a9f7ae35d7ce6d2e8b0752244bd79667f85f5c4e
korrova11 <korrova@yandex.ru>
2024-03-10 19:11:10 +0300

    easy2 commit

--- src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
+++ src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
@@ -9,8 +9,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import pro.sky.telegrambot.cron.NotificationCron;
 import pro.sky.telegrambot.model.Notification_task;
 import pro.sky.telegrambot.repositiries.NotificationRepository;
+import pro.sky.telegrambot.service.NotificationService;
 
 import javax.annotation.PostConstruct;
 import java.time.LocalDateTime;
@@ -27,11 +29,15 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
 
     @Autowired
     private TelegramBot telegramBot;
+    @Autowired
+    private NotificationService notificationService;
+    @Autowired
+    private NotificationCron notificationCron;
 
-    private NotificationRepository repository;
-
-    public TelegramBotUpdatesListener(NotificationRepository repository) {
-        this.repository = repository;
+    public TelegramBotUpdatesListener(TelegramBot telegramBot,NotificationService notificationService, NotificationCron notificationCron) {
+        this.telegramBot=telegramBot;
+        this.notificationService = notificationService;
+        this.notificationCron = notificationCron;
     }
 
     @PostConstruct
@@ -45,23 +51,27 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
             logger.info("Processing update: {}", update);
 
             // Process your updates here
-            Pattern pattern1 = Pattern.compile("([0-9\\.\\:\\s]{16})(\\s)([\\W+]+)");
             if (update.message().text().equals("/start")) {
                 SendMessage message = new SendMessage(update.message().chat().id(),
                         "Здравствуй, " + update.message().chat().firstName()
-                                + "!");
+                                + "! Это напоминалка-бот, чтобы создать напоминание," +
+                                "пришли его в формате: ЧЧ.ММ.ГГГГ  текст напоминания");
                 SendResponse response = telegramBot.execute(message);
             }
+            Pattern pattern1 = Pattern.compile("([0-9\\.\\:\\s]{16})(\\s)([\\W+]+)");
             Matcher matcher = pattern1.matcher(update.message().text());
-            if (matcher.matches()) {
+            else if (matcher.matches()) {
                 String dateTime = matcher.group(1);
-                String text = matcher.group(3);
+                String textMessage = matcher.group(3);
                 LocalDateTime localDateTime = LocalDateTime.parse(dateTime,
                         DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm"));
-               /* repository.save(new Notification_task(update.message().chat().id(),text,
-                       localDateTime ));
-                */
+                Notification_task notification_task = new Notification_task(
+                        1l,update.message().chat().id(),textMessage,localDateTime);
+                notificationService.addInRepo(notification_task);
+
             }
+            else telegramBot.execute(new SendMessage(update.message().chat().id(),
+                        "неправильный формат сообщения"));
 
         });
         return UpdatesListener.CONFIRMED_UPDATES_ALL;
--- src/main/java/pro/sky/telegrambot/service/NotificationService.java
+++ src/main/java/pro/sky/telegrambot/service/NotificationService.java
@@ -4,7 +4,6 @@ import org.springframework.stereotype.Service;
 import pro.sky.telegrambot.model.Notification_task;
 import pro.sky.telegrambot.repositiries.NotificationRepository;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -13,6 +12,7 @@ public class NotificationService {
 private final NotificationRepository repository;
 
     public NotificationService(NotificationRepository repository) {
+
         this.repository = repository;
     }
     public Notification_task addInRepo(Notification_task notification_task){
@@ -24,6 +24,7 @@ private final NotificationRepository repository;
 
     }
     public void deleteFromRepo(Notification_task notification_task){
+
         repository.delete(notification_task);
     }
 
